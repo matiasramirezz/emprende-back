244eace479f757974a735b1cae071addbe8887ba
matiasramirezz <matiasramirez9912@gmail.com>
2024-02-27 20:07:45 -0300

    Nuevo 0.1

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 require("dotenv").config()
 const http= require("http")
 
- function requestcontroller(){
-    //logica muestra funcion 
-    console.log("HOLA MUNDO!!!")
+ function requestcontroller(req, res){
+   const url= req.url
+   const method = req.method
+   
+    console.log({url, method})
+
+    if (method === "GET" && url === "/"){
+      res.setheader("content-type", "text/html; charset=utf-8")
+      res.write("<h1>Hola Mundo Desde la Pagina Principal<h1>")
+      res.end()
+      return 
+   }
  }
  //config nuestro servidor 
     const server= http.createServer(requestcontroller)
